23a8e8688ebdd2cd6c581f6456cfe08b31f41e3d
Gabriel Catice <gabrielcatice@Gabriels-MacBook-Air.local>
2017-10-17 14:17:41 -0200

    i have no idea wtf im doing

---
+++ pokedex3/Model/Pokemon.swift
@@ -0,0 +1,26 @@
+//
+//  Pokemon.swift
+//  pokedex3
+//
+//  Created by Gabriel Catice on 10/17/17.
+//  Copyright © 2017 Gabriel Catice. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
---
+++ pokedex3/View/PokeCell.swift
@@ -0,0 +1,31 @@
+//
+//  PokeCell.swift
+//  pokedex3
+//
+//  Created by Gabriel Catice on 10/17/17.
+//  Copyright © 2017 Gabriel Catice. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLabel.text = self.pokemon.name.capitalized
+        thumbImage.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+}
